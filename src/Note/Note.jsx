a0048f9bcf21b1440c7491d65419b47cd7a31a65
Tejaswini411 <kumartejaswini4@gmail.com>
2020-02-12 11:31:38 -0800

    initial commit

---
+++ src/Note/Note.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Note.css';
+import PropTypes from 'prop-types';
+
+class Note extends Component{
+
+    constructor(props){
+        super(props);
+        this.message ="Hello from Tejas"
+    }
+
+    render(props){
+        return(
+            <div>
+                <h1>{this.message}</h1>
+            </div>
+        )
+    }
+}
+
+export default Note;
